df719c6e393a141ca00596fd32a7f33eb5843c44
Artur Gaetov <agaetov@gmail.com>
2024-02-29 13:51:31 +0300

    homework with pandas

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+COSTUMERS_DATA_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'customers_data.csv')
+EMPLOYEES_DATA_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'employees_data.csv')
+ORDERS_DATA_DIR = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'orders_data.csv')
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import os
+
+import pandas as pd
+import psycopg2
+
+data_path = os.path.dirname(os.path.dirname(__file__))
+
+customers_file = os.path.join(data_path, "homework-1", "north_data", "customers_data.csv")
+employees_file = os.path.join(data_path, "homework-1", "north_data", "employees_data.csv")
+orders_file = os.path.join(data_path, "homework-1", "north_data", "orders_data.csv")
+
+customers = pd.read_csv(customers_file)
+employees = pd.read_csv(employees_file)
+orders = pd.read_csv(orders_file)
+
+with psycopg2.connect(host="localhost", database="north", user="postgres", password="8520") as connector:
+    with connector.cursor() as cursor:
+        for customer in customers.itertuples(index=False):
+            cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)", customer)
+
+        for employee in employees.itertuples(index=False):
+            cursor.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", employee)
+
+        for order in orders.itertuples(index=False):
+            cursor.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", order)
